768243544c6c7240772f239cf973791b382bbb14
unknown <sujoydaspc2023@gmail.com>
2024-12-14 23:51:24 +0600

    first commit

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Navbar</h2>
+            <nav className="">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+                <a href="">Login</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './component/Home/Home.jsx'
+import About from './component/About/About.jsx'
+import Contact from './component/Contact/Contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      },
+    ]
+  },
+
+
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
