8999f8f841069164d9c5f8d39ba33023330f2ed1
unknown <sujoydaspc2023@gmail.com>
2024-12-15 21:27:34 +0600

    Form Making in contact section

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -4,7 +4,11 @@ const Contact = () => {
     <div>
       <h1>Contact Us</h1>
       <p>We are one call away</p>
-      <input type="text" />
+      <form action="">
+        <label htmlFor="">Enter First Name
+          <input type="text" placeholder="Enter First Name" />
+        </label>
+      </form>
     </div>
   )
 }
