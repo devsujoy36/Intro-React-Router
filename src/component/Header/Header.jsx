4baf93d6d5a6933d86dcb50737046d0e56848b10
Sujoy Das <157239662+devsujoy36@users.noreply.github.com>
2024-12-17 19:29:33 +0600

    Update Header.jsx

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,11 +4,13 @@ const Header = () => {
         <div>
             <h2>Navbar</h2>
             <nav className="">
-                <a href="/">Home</a>
+                <ul>
+                    <a href="/">Home</a>
                 <a href="/about">About</a>
                 <a href="/contact">Contact</a>
                 <a href="/signup">Sign Up</a>
                 <a href="">Login</a>
+                </ul>
             </nav>
         </div>
     )
